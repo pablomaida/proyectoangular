50dc54981cb62cd28edcba17a79f45a2440f4ebe
MarcosDetalis <detalismarcos@gmail.com>
2022-04-18 18:21:23 -0400

    news css grid marcos

--- src/app/blognews/blognews.module.ts
+++ src/app/blognews/blognews.module.ts
@@ -2,17 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavigationComponent } from './navigation/navigation.component';
 import { BannerComponent } from './banner/banner.component';
+import { NewsComponent } from './news/news.component';
 
 
 
 @NgModule({
-  declarations: [
-    NavigationComponent,
-    BannerComponent
-  ],
-  imports: [
-    CommonModule
-  ], 
-  exports:[NavigationComponent, BannerComponent]//exportamos pa que pueda llama en el index general 
+  declarations: [NavigationComponent, BannerComponent, NewsComponent],
+  imports: [CommonModule],
+  exports: [NavigationComponent, BannerComponent, NewsComponent], //exportamos pa que pueda llama en el index general
 })
-export class BlognewsModule { }
+export class BlognewsModule {}
