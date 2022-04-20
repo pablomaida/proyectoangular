352fb70a27a2523d441f2e2f7f4085c8fbb12c26
pablomaida <pablomaidana110@gmail.com>
2022-04-19 20:38:44 -0400

    actualizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BlognewsModule } from './blognews/blognews.module';
 
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+   
   ],
   imports: [
     BrowserModule, BlognewsModule
--- src/app/blognews/blognews.module.ts
+++ src/app/blognews/blognews.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { NavigationComponent } from './navigation/navigation.component';
 import { BannerComponent } from './banner/banner.component';
 import { NewsComponent } from './news/news.component';
+import { Banner2Component } from './banner2/banner2.component';
+import { MisionComponent } from './mision/mision.component';
 
 
 
 @NgModule({
-  declarations: [NavigationComponent, BannerComponent, NewsComponent],
+  declarations: [NavigationComponent, BannerComponent, NewsComponent, Banner2Component, MisionComponent],
   imports: [CommonModule],
-  exports: [NavigationComponent, BannerComponent, NewsComponent], //exportamos pa que pueda llama en el index general
+  exports: [NavigationComponent, BannerComponent, NewsComponent,Banner2Component, MisionComponent], //exportamos pa que pueda llama en el index general
 })
 export class BlognewsModule {}
