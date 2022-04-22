21fd654a22f252569bd55a5164aba7014c13f2e2
gustavoriv <gustavorivas346@gmail.com>
2022-04-21 22:05:32 -0400

    op

--- src/app/blognews/blognews.module.ts
+++ src/app/blognews/blognews.module.ts
@@ -5,12 +5,13 @@ import { BannerComponent } from './banner/banner.component';
 import { NewsComponent } from './news/news.component';
 import { Banner2Component } from './banner2/banner2.component';
 import { MisionComponent } from './mision/mision.component';
+import { News2Component } from './news2/news2.component';
 
 
 
 @NgModule({
-  declarations: [NavigationComponent, BannerComponent, NewsComponent, Banner2Component, MisionComponent],
+  declarations: [NavigationComponent, BannerComponent, NewsComponent,News2Component , Banner2Component, MisionComponent],
   imports: [CommonModule],
-  exports: [NavigationComponent, BannerComponent, NewsComponent,Banner2Component, MisionComponent], //exportamos pa que pueda llama en el index general
+  exports: [NavigationComponent, BannerComponent, NewsComponent,News2Component ,Banner2Component, MisionComponent], //exportamos pa que pueda llama en el index general
 })
 export class BlognewsModule {}
---
+++ src/app/blognews/news2/news2.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-news2',
+  templateUrl: './news2.component.html',
+  styleUrls: ['./news2.component.css']
+})
+export class News2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
